9fc14dd37b6b7b2701f2cc232ec569e016870b76
mdiaz <mdiazjusticia@gmail.com>
2016-10-28 20:30:10 +0200

    some initial statistics

--- src/main/services/download.py
+++ src/main/services/download.py
@@ -1,21 +1,22 @@
 import urllib
 import os
 
-def fetch_data(symbol):
-    url = "http://ichart.finance.yahoo.com/table.csv?s="+symbol+\
-    "&amp;d=1&amp;e=1&amp;f=2016&amp;g=d&amp;a=8&amp;b=7&amp;c=2000&amp;ignore=.csv"
+def fetch_data(symbols):
+    for symbol in symbols:
+        url = "http://ichart.finance.yahoo.com/table.csv?s="+symbol+\
+        "&amp;d=1&amp;e=1&amp;f=2016&amp;g=d&amp;a=8&amp;b=7&amp;c=2000&amp;ignore=.csv"
 
-    time_frame = "m" # d -> daily, w -> weekly, m -> monthly.
-    url = "http://real-chart.finance.yahoo.com/table.csv?s="+symbol+\
-            "&a=11&b=22&c=1998&d=09&e=21&f=2016&g="+time_frame+"+&ignore=.csv"
+        time_frame = "m" # d -> daily, w -> weekly, m -> monthly.
+        url = "http://real-chart.finance.yahoo.com/table.csv?s="+symbol+\
+                "&a=11&b=22&c=1998&d=09&e=21&f=2016&g="+time_frame+"+&ignore=.csv"
 
-    fileName = 'C:/data_stock/{}.csv'.format(symbol)
-    if os.path.isfile(fileName):
-        open(fileName, 'a').close()
+        fileName = 'C:/data_stock/{}.csv'.format(symbol)
+        if os.path.isfile(fileName):
+            open(fileName, 'a').close()
 
-    urllib.urlretrieve(url, fileName)
-    print "DEBUG: Downloading for "+symbol
-    print "DEBUG: URL:"+url
+        urllib.urlretrieve(url, fileName)
+        print "DEBUG: Downloading for "+symbol
+        print "DEBUG: URL:"+url
 
 """
 def test_run():
@@ -26,5 +27,6 @@ def test_run():
        fetch_data(symbol) #Download csv for symbol loading.
 test_run()
 """
-fetch_data("EDR.MC")
+symbols = ['SPY', 'GOOG','AAPL','GLD','XOM']
+fetch_data(symbols)
 #fetch_data("GLD")
